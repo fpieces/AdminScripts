418381c0660984e9616eeae65abae8b1260f56f7
Pieces <fp13c3s@gmail.com>
2017-01-11 13:54:53 -0600

    Create Get-NessusScanSummary.ps1

---
+++ PS Scripts/Get-NessusScanSummary.ps1
@@ -0,0 +1,45 @@
+#System Summary from Nessus API
+#Written by @Fpieces
+#01/11/2017
+
+function Get-NessusScanSummary {
+    [cmdletbinding()]
+    param(
+        [parameter(mandatory=$true,position=0)]
+        [string]
+        $Server,
+
+        [parameter(mandatory=$true,position=1)]
+        [int]
+        $Port,
+
+        [parameter(mandatory=$true,position=2)]
+        [pscredential]
+        $Credential 
+    )
+
+    process {
+        #Could not get session to accept valid credentials without using PSCredential.  Setup session similar to : https://github.com/tenable/Posh-Nessus
+        $Session = Invoke-RestMethod -Uri "https://$($Server):$($Port)/session" -Method Post -Body (@{'username'= $Credential.UserName;'password'= $Credential.GetNetworkCredential().Password} | ConvertTo-Json) -ContentType "application/json"
+        
+        #Grab a list of all scans
+        $Scans = Invoke-RestMethod -Uri "https://$($Server):$($Port)/scans" -Method Get -Headers @{'X-Cookie' = "token=$($Session.Token)"}
+
+        #loop through the scans and give summary of output.  In this case, only reviewing Med, High, Critical vulns, but can add low / info counts in the same way.
+        foreach ($Scan in $Scans.scans) {
+            $ScanDetails = Invoke-RestMethod -Uri "https://$($Server):$($Port)/scans/$($Scan.id)" -Method Get -Headers @{'X-Cookie' = "token=$($Session.Token)"}
+
+            $CriticalVulnCount = 0
+            $HighVulnCount = 0
+            $MediumVulnCount = 0
+
+            foreach ($HostDetail in $ScanDetails.Hosts) {
+                $CriticalVulnCount += $HostDetail.critical
+                $HighVulnCount += $HostDetail.high
+                $MediumVulnCount += $HostDetail.medium
+            }
+    
+            Write-Output (New-Object PSObject -Property @{ Scan = $Scan.name; Critical = $CriticalVulnCount; High = $HighVulnCount; Medium = $MediumVulnCount } | Select Scan, Critical, High, Medium)
+        }
+    }
+}
